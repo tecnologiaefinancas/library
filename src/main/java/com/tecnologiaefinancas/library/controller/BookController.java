2e90ed1cb8baaaa7d5a3e33dffd5ee34129027ca
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-01-13 11:01:37 -0300

    First commit

---
+++ src/main/java/com/tecnologiaefinancas/library/controller/BookController.java
@@ -0,0 +1,31 @@
+package com.tecnologiaefinancas.library.controller;
+
+
+import com.tecnologiaefinancas.library.model.Book;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private List<Book> books = new ArrayList<>();
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        books.add(book);
+        return book;
+    }
+
+    @PostConstruct
+    public void init() { books.add(new Book(1L, "The Great Gatsby", "F. Scott Fitzgerald")); books.add(new Book(2L, "1984", "George Orwell")); books.add(new Book(3L, "To Kill a Mockingbird", "Harper Lee")); }
+}
+
