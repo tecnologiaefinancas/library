ce7c6b996b362622375c423d8d5c539fef3b5081
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-02-19 17:06:39 -0300

    Add EnableMongloRepositories

--- src/main/java/com/tecnologiaefinancas/library/LibraryApplication.java
+++ src/main/java/com/tecnologiaefinancas/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.tecnologiaefinancas.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.tecnologiaefinancas.library.repository")
 public class LibraryApplication {
 
     public static void main(String[] args) {
