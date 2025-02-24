9fab23fd0805f5708ab582cefc1576651032f843
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-02-24 11:52:37 -0300

    Improving MongoConfig

--- src/main/java/com/tecnologiaefinancas/library/config/MongoConfig.java
+++ src/main/java/com/tecnologiaefinancas/library/config/MongoConfig.java
@@ -1,11 +1,22 @@
 package com.tecnologiaefinancas.library.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-public class MongoConfig {
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
 
     @Bean
     public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
