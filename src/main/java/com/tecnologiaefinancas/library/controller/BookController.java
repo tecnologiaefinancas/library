0f3624b03ee3932bc5c829e9fc3e1b07524f0e50
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-03-05 09:54:55 -0300

    Add getBookById and updateBook

--- src/main/java/com/tecnologiaefinancas/library/controller/BookController.java
+++ src/main/java/com/tecnologiaefinancas/library/controller/BookController.java
@@ -3,15 +3,18 @@ package com.tecnologiaefinancas.library.controller;
 
 
 import com.tecnologiaefinancas.library.entity.Book;
+import com.tecnologiaefinancas.library.excepction.ResourceNotFoundException;
 import com.tecnologiaefinancas.library.repository.BookRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -26,9 +29,27 @@ public class BookController {
         return bookRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookRepository.findById(id);
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public Book createBook(@RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Book> updtateBook(@PathVariable Long id, @RequestBody Book bookDetails){
+        {
+            Book book = bookRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Id not found."));
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            Book updatedBook = bookRepository.save(book);
+            return ResponseEntity.ok(updatedBook);
+        }
     }
 
 
