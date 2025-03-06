c9633bbec385035b44febd59e2717c284a2074a3
Natalia Feitosa <estudandocertificacoes@gmail.com>
2025-03-06 16:41:49 -0300

    Add Mockito and Unitary Tests

--- src/main/java/com/tecnologiaefinancas/library/repository/BookRepository.java
+++ src/main/java/com/tecnologiaefinancas/library/repository/BookRepository.java
@@ -2,10 +2,11 @@ package com.tecnologiaefinancas.library.repository;
 
 import com.tecnologiaefinancas.library.entity.Book;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long > {
+public interface BookRepository extends JpaRepository<Book, Long > {
 
 }
